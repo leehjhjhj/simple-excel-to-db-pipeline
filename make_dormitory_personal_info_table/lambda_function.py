904d0bfa73460633cc7540f9a0356a8c76894b18
leehjhjhj <tsukiakarii@naver.com>
2024-02-28 00:53:19 +0900

    feat: xls 파일 분석 기능 추가

--- make_dormitory_personal_info_table/lambda_function.py
+++ make_dormitory_personal_info_table/lambda_function.py
@@ -13,8 +13,14 @@ def send_event_data_to_make_member_event_lambda_function(event_data):
     )
 
 def lambda_handler(event, context):
-    xls_file = get_xls_file_from_s3(event)
-    data = pd.read_excel(xls_file, engine='openpyxl')
+    xls_file, file_extension = get_xls_file_from_s3(event)
+    
+    data = None
+    if file_extension == '.xlsx':
+        data = pd.read_excel(xls_file, engine='openpyxl')
+    elif file_extension == '.xls':
+        data = pd.read_excel(xls_file, engine='xlrd')
+
     data = data.dropna()
     event_data = make_personal_data(data)
     send_event_data_to_make_member_event_lambda_function(event_data)

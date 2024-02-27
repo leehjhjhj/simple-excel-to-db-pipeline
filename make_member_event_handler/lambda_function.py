c9527207c5bce16bf3e58bf2c28c0c354729eb57
leehjhjhj <tsukiakarii@naver.com>
2024-02-27 10:35:19 +0900

    refactor: 기존 회원 검사 로직을 변경하여 성능 개선

--- make_member_event_handler/lambda_function.py
+++ make_member_event_handler/lambda_function.py
@@ -16,12 +16,15 @@ def lambda_handler(event, context):
     )
     event_data = event
     with conn.cursor() as cursor:
+        # 기존 회원들의 username을 set에 담기
+        cursor.execute("SELECT username FROM domtory.member")
+        existing_usernames = set(row['username'] for row in cursor.fetchall())
+    
         # 새로운 회원 데이터를 추가
         for data in event_data:
             id, dormitory_code, name, phone_number, birthday = data
-            # 기존 회원 테이블에서 일치하는 회원 정보가 없는 경우에만 추가
-            cursor.execute("SELECT * FROM domtory.member WHERE username = %s", (dormitory_code,))
-            if not cursor.fetchone():
+            # 기존 회원 username에서 일치하는 회원 정보가 없는 경우에만 추가
+            if dormitory_code not in existing_usernames:
                 cursor.execute("INSERT INTO domtory.member (username, password, name, phone_number, birthday, status) VALUES (%s, %s, %s, %s, %s, 'ACTIVE')", (dormitory_code, birthday, name, phone_number, birthday))
         conn.commit()
         

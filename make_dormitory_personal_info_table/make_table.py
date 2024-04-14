602e3e617851f5b4a706a4c3d4e74ab218dfce13
leehjhjhj <tsukiakarii@naver.com>
2024-04-14 09:33:21 +0900

    refactor: 엑셀 정보에서 No를 ID로 치환하는 부분 삭제

--- make_dormitory_personal_info_table/make_table.py
+++ make_dormitory_personal_info_table/make_table.py
@@ -21,18 +21,17 @@ def make_personal_data(data):
         event_data = []
         for index, row in data.iterrows():
             try:
-                id = int(row['No'])
                 dormitory_code = row['학사번호']
                 room_number = row['사실']
                 name = row['성명']
                 phone_number = row['휴대폰']
                 birthday = int(row['생년월일'])
                 sql = """
-                INSERT INTO domtory.dormitory_personal_info (id, dormitory_code, room_number, name, phone_number, birthday)
-                VALUES (%s, %s, %s, %s, %s, %s)
+                INSERT INTO domtory.dormitory_personal_info (dormitory_code, room_number, name, phone_number, birthday)
+                VALUES ( %s, %s, %s, %s, %s)
                 """
-                cursor.execute(sql, (id, dormitory_code, room_number, name, phone_number, birthday))
-                event_data.append((id, dormitory_code, name, phone_number, birthday))
+                cursor.execute(sql, (dormitory_code, room_number, name, phone_number, birthday))
+                event_data.append((dormitory_code, name, phone_number, birthday))
             except Exception as e:
                 logging.error(f'DB 저장 오류 발생: {e}')
         conn.commit()
